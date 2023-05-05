2f3d9253df0f2f4177036eaea13bd306ddc0b489
faisal <isaladha0@gmail.com>
2023-05-05 18:47:58 +0700

    [update]mengganti nama fungsi

--- header/Faisal_dev.h
+++ header/Faisal_dev.h
@@ -7,6 +7,7 @@
 #ifndef Faisal_dev_H
 #define Faisal_dev_H
 #include "adinda_dev.h"
+#include "AhmadFauzy_dev.h"
 
 //operasi pangkat
  /*Fungsi operasi pangkat untuk dua bilangan.
@@ -16,10 +17,10 @@
  lalu mengembalikan hasil operasi pangkat bilangansatu
  dengan pangkat bilangandua.*/
 
-double operasiPangkat(double bilangansatu,double bilangandua){
+double operasi_pangkat(double bilangansatu,double bilangandua){
     double a,total=1;
     for(a=0;a<bilangandua;a++){
-        total = operasiPerkalian(total, bilangansatu);// menggunakan fungsi operasiPerkalian dari adinda_dev.h
+        total = operasi_perkalian(total, bilangansatu);// memanggil fungsi operasi_perkalian dari adinda_dev.h
     }
     return total;
 
@@ -31,8 +32,8 @@ double operasiPangkat(double bilangansatu,double bilangandua){
  yaitu bilangan yang akan diubah dari nilai persentase
  menjadi nilai desimal, lalu mengembalikan hasil pembagian bilangan tersebut dengan 100.*/
 
-double operasipersen(double bilangan){
-	return operasiPembagian(bilangan, 100.0);//menggunakan fungsi operasiPembagian dari AhmadFauzy_dev,h
+double operasi_persen(double bilangan){
+	return operasi_pembagian(bilangan, 100.0);//memanggil fungsi operasi_pembagian dari AhmadFauzy_dev,h
 }
 
 #endif

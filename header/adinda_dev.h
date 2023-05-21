720746f814c80ca225f1d6f36184f378bce52e5f
adindaraisa <adindazhr11@gmail.com>
2023-05-21 12:30:13 +0700

    clean code header adinda_dev

--- header/adinda_dev.h
+++ header/adinda_dev.h
@@ -1,3 +1,16 @@
+/*
+dibuat oleh : Adinda Raisa Az-zahra
+Deskripsi : Memuat proses perhitungan:
+1. operasi perkalian
+2. operasi sinus
+3. operasi cosinus
+4. operasi tangen
+5. operasi cosecan
+6. operasi secan
+7. operasi cotangen
+
+*/
+
 #include<stdio.h>
 
 #include <math.h>
@@ -12,95 +25,117 @@ double operasi_perkalian(double bilangansatu, double bilangandua) {
     return bilangansatu * bilangandua;
 }
 
-double operasiSinus(double bilangan) { // menghitung nilai sinus dari suatu sudut
-    double radian = bilangan * (PI / 180); // inputan menggunakan satuan derajat sehigga perlu dikonversi terlebih dahulu menuju radian
-    double jumlah = 0.0; // untuk menghitung nilai jumlah semua sukunya
-    double nilaipersuku = radian; // nilaipersuku adalah nilai yang dihasilkan pengoperasian per sukunya
+
+/* fungsi operasi sinus */
+/* menghitung nilai sinus dari suatu sudut */
+double operasiSinus(double bilangan) { 
+	/* inputan menggunakan satuan derajat sehigga perlu dikonversi terlebih dahulu menuju radian */
+    double radian = bilangan * (PI / 180); 
+    /* untuk menghitung nilai jumlah semua sukunya */
+    double jumlah = 0.0; 
+    /* nilaipersuku adalah nilai yang dihasilkan pengoperasian per sukunya */
+    double nilaipersuku = radian; 
 
     int n = 1;
     while (nilaipersuku != 0.0) {
         jumlah += nilaipersuku;
-        n += 2; // ditambahkan dengan 2 karena suku yg digunakan adalah suku ganjil sebab turunan ganjil merupakan sinus itu sendiri
-        nilaipersuku = (-1.0) * nilaipersuku * radian * radian / ((n - 1) * n); //menggunakan deret taylor
+        /* ditambahkan dengan 2 karena suku yg digunakan adalah suku ganjil sebab turunan ganjil merupakan sinus itu sendiri */
+        n += 2; 
+        /* menggunakan deret taylor */
+        nilaipersuku = (-1.0) * nilaipersuku * radian * radian / ((n - 1) * n); 
     }
+    /* mengambalikan jumlah sebagai hasil operasi sinus */
     return jumlah;
 }
 
-double operasiCosinus(double bilangan) { // menghitung nilai cosinus dari suatu sudut
-    double radian = bilangan * (PI / 180); // inputan menggunakan satuan derajat sehigga perlu dikonversi terlebih dahulu menuju radian
-    double jumlah = 0.0; // untuk menghitung nilai jumlah semua sukunya
-    double nilaipersuku = 1; // nilaipersuku adalah nilai yang dihasilkan pengoperasian per sukunya
+/* fungsi operasi cosinus */
+/* menghitung nilai cosinus dari suatu sudut */
+double operasiCosinus(double bilangan) { 
+	/* inputan menggunakan satuan derajat sehigga perlu dikonversi terlebih dahulu menuju radian */
+    double radian = bilangan * (PI / 180); 
+    /* untuk menghitung nilai jumlah semua sukunya */
+    double jumlah = 0.0; 
+    /* nilaipersuku adalah nilai yang dihasilkan pengoperasian per sukunya */
+    /* dimulai dari satu untuk operasi cosinus */
+    double nilaipersuku = 1; 
 
-    int n = 0; // dimulai dari nol karena yang digunakan suku genap, turunan genap untuk cosinus merupakan cosinus itu sendiri
+	/* dimulai dari nol karena yang digunakan suku genap, turunan genap untuk cosinus merupakan cosinus itu sendiri */
+    int n = 0; 
     while (nilaipersuku != 0.0) {
         jumlah += nilaipersuku;
+        /* ditambahkan dengan 2 karena suku yg digunakan adalah suku genap sebab turunan genap merupakan cosinus itu sendiri */
         n += 2;
+        /* menggunakan deret taylor */
         nilaipersuku = (-1.0) * nilaipersuku * radian * radian / ((n - 1) * n);
     }
+    /* mengambalikan jumlah sebagai hasil operasi sinus */
     return jumlah;
 }
 
+/* fungsi operasi tangen */
+/* menghitung nilai tangen dari suatu sudut */
 double operasiTangen(double bilangan){
 	double result;
-	if(bilangan==90 || bilangan==270){
+	
+	/* pengecualian pada beberapa case karena 1/0 hasilnya tidak terdefinisi */
+	if(bilangan==90 || bilangan==270){ 
 		printf("Not Defined \n");
 		exit(1);
 	} else{
-		result=operasi_pembagian(operasiSinus(bilangan),operasiCosinus(bilangan)); //tangen merupakan hasil bagi sin dengan cos
+		/* tangen merupakan hasil bagi sin dengan cos */
+		result=operasi_pembagian(operasiSinus(bilangan),operasiCosinus(bilangan)); 
+		/* mengembalikan hasil pembagian */
 		return (result);
 	}
 	
 }
 
 
-
+/* fungsi operasi cosecan */
+/* menghitung nilai cosecan dari suatu sudut */
 double operasiCosecan(double bilangan) {
     double result;
+    /* pengecualian pada beberapa case karena 1/0 hasilnya tidak terdefinisi */
     if (bilangan == 0 || bilangan == 180 || bilangan == 360) {
         printf("Not Defined \n");
     } else {
-        result = operasi_pembagian(1, operasiSinus(bilangan)); // cosecan merupakan satu per dari sinus
+    	/* cosecan merupakan satu per dari sinus */
+        result = operasi_pembagian(1, operasiSinus(bilangan)); 
+        /* mengembalikan hasil pembagian */
         return (result);
     }
 
 }
 
+/* fungsi operasi secan */
+/* menghitung nilai secan dari suatu sudut */
 double operasiSecan(double bilangan) {
     double result;
+    /* pengecualian pada beberapa case karena 1/0 hasilnya tidak terdefinisi */
     if (bilangan == 90 || bilangan == 270) {
         printf("Not Defined \n");
     } else {
-        result = operasi_pembagian(1, operasiCosinus(bilangan)); //tangen merupakan hasil bagi sin dengan cos
+    	/* secan merupakan satu per dari cosinus */
+        result = operasi_pembagian(1, operasiCosinus(bilangan)); 
+        /* mengembalikan hasil pembagian */
         return (result);
     }
 }
 
+/* fungsi operasi cotangen */
+/* menghitung nilai cotangen dari suatu sudut */
 double operasiCotangen(double bilangan) {
     double result;
+    /* pengecualian pada beberapa case karena 1/0 hasilnya tidak terdefinisi */
     if (bilangan == 0 || bilangan == 180 || bilangan == 360) {
         printf("Not Defined \n");
     } else {
-        result = operasi_pembagian(1, operasiTangen(bilangan)); // cosecan merupakan satu per dari sinus
+    	/* cotangen merupakan satu per dari tangen */
+        result = operasi_pembagian(1, operasiTangen(bilangan)); 
+        /* mengembalikan hasil pembagian */
         return (result);
     }
 }
 
-double konversi_fahrenheit(double bilangan) {
-    double result;
-    result = (9.0 / 5.0 * bilangan) + 32;
-    return result;
-}
-
-double konversi_kelvin(double bilangan) {
-    double result;
-    result = bilangan + 273.15;
-    return result;
-}
-
-float konversi_reamur(float bilangan) {
-    float result;
-    result = bilangan * (4.0 / 5.0);
-    return result;
-}
 
 #endif
